998e46f6b75413cdf2712d9915f61666c13e6ff6
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-13 20:07:13 +0330

    Support for Add, Update Delete and filter of orders in UI Component

---
+++ src/CSharp/Cores/EasyMicroservices.UI.Ordering.ViewModels/ViewModels/AddOrUpdateOrderViewModel.cs
@@ -0,0 +1,127 @@
+﻿using EasyMicroservices.ServiceContracts;
+using EasyMicroservices.UI.Core;
+using EasyMicroservices.UI.Core.Commands;
+using Ordering.GeneratedServices;
+
+namespace EasyMicroservices.UI.Ordering.ViewModels
+{
+    public class AddOrUpdateOrderViewModel : BaseViewModel
+    {
+        public AddOrUpdateOrderViewModel(OrderClient orderClient)
+        {
+            _orderClient = orderClient;
+            SaveCommand = new TaskRelayCommand(this, Save);
+            Clear();
+        }
+
+        public TaskRelayCommand SaveCommand { get; set; }
+
+        readonly OrderClient _orderClient;
+
+        public Action OnSuccess { get; set; }
+        OrderContract _UpdateOrderContract;
+        /// <summary>
+        /// for update
+        /// </summary>
+        public OrderContract UpdateOrderContract
+        {
+            get
+            {
+                return _UpdateOrderContract;
+            }
+            set
+            {
+                if (value is not null)
+                {
+                    Name = value.Name;
+                    PriceAmount = value.Price.Amount;
+                }
+                _UpdateOrderContract = value;
+            }
+        }
+
+        string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+
+        decimal _PriceAmount;
+        public decimal PriceAmount
+        {
+            get => _PriceAmount;
+            set
+            {
+                _PriceAmount = value;
+                OnPropertyChanged(nameof(PriceAmount));
+            }
+        }
+
+        public async Task Save()
+        {
+            if (UpdateOrderContract is not null)
+                await UpdateOrder();
+            else
+                await AddOrder();
+            OnSuccess?.Invoke();
+        }
+
+        public async Task AddOrder()
+        {
+            await _orderClient.AddAsync(new CreateOrderRequestContract()
+            {
+                Price = GetPrice(),
+                Names = GetNames(),
+            }).AsCheckedResult(x => x.Result);
+            Clear();
+        }
+
+        public async Task UpdateOrder()
+        {
+            await _orderClient.UpdateAsync(new UpdateOrderRequestContract()
+            {
+                Id = UpdateOrderContract.Id,
+                ParentId = _UpdateOrderContract.ParentId,
+                ProductId = _UpdateOrderContract.ProductId,
+                UniqueIdentity = _UpdateOrderContract.UniqueIdentity,
+                Price = GetPrice(),
+                Names = GetNames(),
+            }).AsCheckedResult(x => x.Result);
+            Clear();
+        }
+
+        PriceContract GetPrice()
+        {
+            return new PriceContract()
+            {
+                Amount = PriceAmount,
+                CurrencyCode = CurrencyCodeType.IRR
+            };
+        }
+
+        List<LanguageDataContract> GetNames()
+        {
+            return new List<LanguageDataContract>()
+            {
+                new LanguageDataContract()
+                {
+                    Data = Name,
+                    Language = "fa-IR"
+                }
+            };
+        }
+
+        public void Clear()
+        {
+            Name = "";
+            PriceAmount = 0;
+            UpdateOrderContract = default;
+        }
+    }
+}
---
+++ src/CSharp/Cores/EasyMicroservices.UI.Ordering.ViewModels/ViewModels/FilterOrdersListViewModel.cs
@@ -0,0 +1,76 @@
+﻿using EasyMicroservices.ServiceContracts;
+using EasyMicroservices.UI.Core;
+using EasyMicroservices.UI.Core.Commands;
+using Ordering.GeneratedServices;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using System.Windows.Input;
+
+namespace EasyMicroservices.UI.Ordering.ViewModels
+{
+    public class FilterOrdersListViewModel : BaseViewModel
+    {
+        public FilterOrdersListViewModel(OrderClient orderClient)
+        {
+            _orderClient = orderClient;
+            SearchCommand = new TaskRelayCommand(this, Search);
+            DeleteCommand = new TaskRelayCommand<OrderContract>(this, Delete);
+            SearchCommand.Execute(null);
+        }
+
+
+        public ICommand SearchCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public Action<OrderContract> OnDelete { get; set; }
+        readonly OrderClient _orderClient;
+        OrderContract _SelectedOrderContract;
+        public OrderContract SelectedOrderContract
+        {
+            get =>_SelectedOrderContract;
+            set
+            {
+                _SelectedOrderContract = value;
+                OnPropertyChanged(nameof(SelectedOrderContract));
+            }
+        }
+
+        public ObservableCollection<OrderContract> Orders { get; set; } = new ObservableCollection<OrderContract>();
+
+        private async Task Search()
+        {
+            var filteredResult = await _orderClient.FilterAsync(new FilterRequestContract()
+            {
+                IsDeleted = false
+            }).AsCheckedResult(x => (x.Result, x.TotalCount));
+
+            Orders.Clear();
+
+            foreach (var order in filteredResult.Result)
+            {
+                Orders.Add(order);
+            }
+        }
+
+        public async Task Delete(OrderContract contract)
+        {
+            await _orderClient.SoftDeleteByIdAsync(new Int64SoftDeleteRequestContract()
+            {
+                Id = contract.Id,
+                IsDelete = true
+            }).AsCheckedResult(x => x);
+            Orders.Remove(contract);
+            OnDelete?.Invoke(contract);
+        }
+
+        public override Task OnError(Exception exception)
+        {
+            return base.OnError(exception);
+        }
+
+        public override Task DisplayFetchError(ServiceContracts.ErrorContract errorContract)
+        {
+            return base.DisplayFetchError(errorContract);
+        }
+    }
+}
